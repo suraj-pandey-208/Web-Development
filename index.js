66a6da9f86e8cdbc5ab58ac6e607388762b84068
Suraj Pandey <surajpandey0289@gmail.com>
2026-01-28 16:29:08 +0530

    declare var let const

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello")
+console.log(3 + 4)
+let a = 10;
+let b = 10;
+console.log(a, b)
+
+// const a=10
+// const b=10
+// console.log(a,b)
+
+
+// var a = 10
+// var b = 10
+// console.log(a, b)
