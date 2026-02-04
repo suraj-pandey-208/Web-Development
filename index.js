300c072c9662852087e62745b2ef69f8b860d3fb
Suraj Pandey <surajpandey0289@gmail.com>
2026-02-04 15:54:21 +0530

    change in index

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-console.log("hello")
-console.log(3 + 4)
-let a = 10;
-let b = 10;
-console.log(a, b)
+// console.log("hello")
+// console.log(3 + 4)
+// let a = 10;
+// let b = 10;
+// console.log(typeof(a))
+
 
 // const a=10
-// const b=10
+// const b=10;
+
 // console.log(a,b)
 
+var x=10.0
+let b=true
+const y=30
 
-// var a = 10
-// var b = 10
-// console.log(a, b)
+// console.log(a,b,c)
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+
+// console.log(x+y)
+// console.log(x-y)
+
+// console.log(x*y)
+
+// console.log(x/y)
+// let arr=[10,20,30,40]
+// console.log(arr[0])
+// arr.push(90)
+// arr.pop()
+// console.log(arr)
+let students={
+    name:"suraj",
+    age:20
+}
+console.log(students.name)
+
+const add=(p,q)=>p*q
+console.log(add(8,9))
+
+let today=new Date()
+console.log(today)
+
+console.log(Math.sqrt(x))
+
+let a="hello"
+
+let strNum="123"
+let convert=Number(strNum)
+console.log(convert+10)
